20673b0f3c1beeff13f97fc4efcd6cef828c571c
Guillaume BERT <guillaume.bert13@gmail.com>
2020-10-23 19:42:23 +0100

    add https DNS for godgiven.fr

--- arm32v7/init-letsencrypt.sh
+++ arm32v7/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(lalainaetguillaume.zapto.org)
+domains=(lalainaetguillaume.zapto.org godgiven.fr www.godgiven.fr)
 rsa_key_size=4096
 data_path="./front/nginx/certbot"
 email="guillaume.bert13@gmail.com" # Adding a valid address is strongly recommended
