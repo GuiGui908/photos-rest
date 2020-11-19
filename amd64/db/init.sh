d73276802e1e2a75985b05844b9fcf6436f2bbe1
Guillaume BERT <guillaume.bert13@gmail.com>
2020-11-19 13:01:26 +0000

    add memories database in init script

--- amd64/db/init.sh
+++ amd64/db/init.sh
@@ -3,5 +3,8 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER portal_owner WITH ENCRYPTED PASSWORD 'superSecuredPassword';
+    CREATE USER memories_owner WITH ENCRYPTED PASSWORD 'superSecuredPassword';
     GRANT ALL PRIVILEGES ON DATABASE db_portal TO portal_owner;
+    create database db_memories;
+    GRANT ALL PRIVILEGES ON DATABASE db_memories TO memories_owner;
 EOSQL
